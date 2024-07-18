a1da78efb95f007721f49c6d04bdbaf8fc85a3f6
ahmetakay24 <ahmetakay6600@gmail.com>
2024-07-18 21:55:08 +0300

    UI of MenuView was finished

--- lib/screens/menu/view/menu_view.dart
+++ lib/screens/menu/view/menu_view.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:fashion_e_commerce_app/core/colors/core_app_colors.dart';
 import 'package:fashion_e_commerce_app/core/size/device_size.dart';
+import 'package:fashion_e_commerce_app/core/widgets/app_custom_widgets.dart';
 import 'package:fashion_e_commerce_app/core/widgets/app_images.dart';
 import 'package:fashion_e_commerce_app/screens/menu/view/widgets/buttons/menu_buttons.dart';
 import 'package:fashion_e_commerce_app/screens/menu/view/widgets/category_titles/menu_category_titles.dart';
@@ -47,22 +48,96 @@ class MenuView extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(left: sizeCalculator(size.width, 5.06)),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  height: sizeCalculator(size.height, 5.46875),
-                  width: sizeCalculator(size.width, 42.93),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: sizeCalculator(size.height, 5.46875),
-                        width: sizeCalculator(size.width, 6.39),
-                        child: const AppIcons(icon: "Call"),
-                      )
-                    ],
+              padding: EdgeInsets.only(left: sizeCalculator(size.width, 8.90), bottom: sizeCalculator(size.height, 3)),
+              child: InkWell(
+                onTap: null,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: SizedBox(
+                    height: sizeCalculator(size.height, 5.46875),
+                    width: sizeCalculator(size.width, 42.93),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: sizeCalculator(size.height, 3.75),
+                          width: sizeCalculator(size.width, 6.39),
+                          child: const AppIcons(icon: "Call"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: sizeCalculator(size.width, 4.26)),
+                          child: SizedBox(
+                            height: sizeCalculator(size.height, 5.46875),
+                            width: sizeCalculator(size.width, 32.26),
+                            child: const FittedBox(fit: BoxFit.fitWidth, child: Text("(786) 713-8616")),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: sizeCalculator(size.width, 8.90), bottom: sizeCalculator(size.height, 4.296875)),
+              child: InkWell(
+                onTap: null,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: SizedBox(
+                    height: sizeCalculator(size.height, 5.46875),
+                    width: sizeCalculator(size.width, 37.59),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: sizeCalculator(size.height, 3.75),
+                          width: sizeCalculator(size.width, 6.39),
+                          child: const AppIcons(icon: "Location"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: sizeCalculator(size.width, 4.26)),
+                          child: SizedBox(
+                            height: sizeCalculator(size.height, 5.46875),
+                            width: sizeCalculator(size.width, 26.93),
+                            child: const FittedBox(fit: BoxFit.fitWidth, child: Text("Store Locator")),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: sizeCalculator(size.height, 3.734375)),
+              child: AppHeaderLine(size: size, colors: colors),
+            ),
+            SizedBox(
+              height: sizeCalculator(size.height, 3.75),
+              width: sizeCalculator(size.width, 37.86),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: sizeCalculator(size.height, 3.75),
+                    width: sizeCalculator(size.width, 6.39),
+                    child: const AppIcons(icon: "x"),
+                  ),
+                  SizedBox(
+                    height: sizeCalculator(size.height, 3.75),
+                    width: sizeCalculator(size.width, 6.39),
+                    child: const AppIcons(icon: "instagram_vector"),
+                  ),
+                  SizedBox(
+                    height: sizeCalculator(size.height, 3.75),
+                    width: sizeCalculator(size.width, 6.39),
+                    child: const AppIcons(icon: "youtube"),
+                  ),
+                ],
               ),
             )
           ],
